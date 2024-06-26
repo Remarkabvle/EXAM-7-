d7857efc48f96754eeb301783645bcc0d8b7a6b0
Abubakr <abubakrdev69@gmail.com>
2024-06-26 23:15:50 +0500

    add all pages

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import HeroSection from '../../components/hero/HeroSection'
+import Item from '../../components/item/Item'
+
+function Home() {
+  return (
+    <div>
+      <HeroSection/>
+      <Item/>
+    </div>
+  )
+}
+
+export default Home
